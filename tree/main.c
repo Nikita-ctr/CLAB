c762af7a062e80f1ba277655f63d87f4b63a9f9d
Nikita-ctr <xmil.nik@gmail.com>
2022-05-11 23:20:46 +0300

    lab_5

---
+++ tree/main.c
@@ -0,0 +1,197 @@
+#include <malloc.h>
+#include <stdlib.h>
+
+typedef int T;
+
+#define CMP_EQ(a, b) ((a) == (b))
+#define CMP_LT(a, b) ((a) < (b))
+#define CMP_GT(a, b) ((a) > (b))
+
+typedef struct Node {
+    T data;
+    struct Node *left;
+    struct Node *right;
+    struct Node *parent;
+} Node;
+
+//Функция которая создает новый узел
+Node* getFreeNode(T value, Node *parent) {
+    Node* tmp = (Node*) malloc(sizeof(Node));
+    tmp->left = tmp->right = NULL;
+    tmp->data = value;
+    tmp->parent = parent;
+    return tmp;
+}
+
+//Функция, добавляющая узел в дерево
+void insert(Node **head, int value) {
+    Node *tmp = NULL;
+    Node *ins = NULL;
+    if (*head == NULL) {
+        *head = getFreeNode(value, NULL);
+        return;
+    }
+
+    tmp = *head;
+    while (tmp) {
+        if (CMP_GT(value, tmp->data)) {
+            if (tmp->right) {
+                tmp = tmp->right;
+                continue;
+            } else {
+                tmp->right = getFreeNode(value, tmp);
+                return;
+            }
+        } else if (CMP_LT(value, tmp->data)) {
+            if (tmp->left) {
+                tmp = tmp->left;
+                continue;
+            } else {
+                tmp->left = getFreeNode(value, tmp);
+                return;
+            }
+        } else {
+            exit(2);
+        }
+    }
+}
+
+
+Node* getMinNode(Node *root) {
+    while (root->left) {
+        root = root->left;
+    }
+    return root;
+}
+
+
+Node* getMaxNode(Node *root) {
+    while (root->right) {
+        root = root->right;
+    }
+    return root;
+}
+
+
+Node *getNodeByValue(Node *root, T value) {
+    while (root) {
+        if (CMP_GT(root->data, value)) {
+            root = root->left;
+            continue;
+        } else if (CMP_LT(root->data, value)) {
+            root = root->right;
+            continue;
+        } else {
+            return root;
+        }
+    }
+    return NULL;
+}
+
+
+void removeNodeByPtr(Node *target) {
+    if (target->left && target->right) {
+        Node *localMax = getMaxNode(target->left);
+        target->data = localMax->data;
+        removeNodeByPtr(localMax);
+        return;
+    } else if (target->left) {
+        if (target == target->parent->left) {
+            target->parent->left = target->left;
+        } else {
+            target->parent->right = target->left;
+        }
+    } else if (target->right) {
+        if (target == target->parent->right) {
+            target->parent->right = target->right;
+        } else {
+            target->parent->left = target->right;
+        }
+    } else {
+        if (target == target->parent->left) {
+            target->parent->left = NULL;
+        } else {
+            target->parent->right = NULL;
+        }
+    }
+    free(target);
+}
+
+
+void deleteValue(Node *root, T value) {
+    Node *target = getNodeByValue(root, value);
+    removeNodeByPtr(target);
+}
+
+
+int _print_t(Node *tree, int is_left, int offset, int depth, char s[20][255])
+{
+    char b[20];
+    int width = 5;
+
+    if (!tree) return 0;
+
+    sprintf(b, "(%03d)", tree->data);
+
+    int left  = _print_t(tree->left,  1, offset,                depth + 1, s);
+    int right = _print_t(tree->right, 0, offset + left + width, depth + 1, s);
+
+
+    for (int i = 0; i < width; i++)
+        s[2 * depth][offset + left + i] = b[i];
+
+    if (depth && is_left) {
+
+        for (int i = 0; i < width + right; i++)
+            s[2 * depth - 1][offset + left + width/2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width/2] = '+';
+        s[2 * depth - 1][offset + left + width + right + width/2] = '+';
+
+    } else if (depth && !is_left) {
+
+        for (int i = 0; i < left + width; i++)
+            s[2 * depth - 1][offset - width/2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width/2] = '+';
+        s[2 * depth - 1][offset - width/2 - 1] = '+';
+    }
+
+
+    return left + width + right;
+}
+
+void print_t(Node *tree)
+{
+    char s[20][255];
+    for (int i = 0; i < 20; i++)
+        sprintf(s[i], "%80s", " ");
+
+    _print_t(tree, 0, 0, 0, s);
+
+    for (int i = 0; i < 20; i++)
+        printf("%s\n", s[i]);
+}
+
+
+
+int main() {
+    Node *root = NULL;
+    insert(&root, 10);
+    insert(&root, 12);
+    insert(&root, 8);
+    insert(&root, 9);
+    insert(&root, 7);
+    insert(&root, 3);
+    insert(&root, 4);
+    print_t(root);
+    printf("max = %d\n", getMaxNode(root)->data);
+    printf("min = %d\n", getMinNode(root)->data);
+    deleteValue(root, 12);
+    printf("parent of 7 is %d\n", getNodeByValue(root, 7)->parent->data);
+    print_t(root);
+    printf("max = %d\n", getMaxNode(root)->data);
+    printf("min = %d\n", getMinNode(root)->data);
+
+
+}
